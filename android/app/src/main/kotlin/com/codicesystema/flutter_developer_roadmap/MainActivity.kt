ed244c0955473e4ab231b7ab4cbbf451830c34c9
Davson Oldac Damasceno Machado <davsonirom@me.com>
2022-11-11 00:26:42 -0300

    roadmap

---
+++ android/app/src/main/kotlin/com/codicesystema/flutter_developer_roadmap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codicesystema.flutter_developer_roadmap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const CodicesSystema());
+
+class CodicesSystema extends StatelessWidget {
+  const CodicesSystema({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
